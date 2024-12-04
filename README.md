4b415c85ae6fffe3dec250e4d436bc14103b7e52
mohammed alahmadi <alahmadimohammed0@gmail.com>
2024-12-03 20:08:12 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # comp002.assigment9
+author: mohammed ali
+purpose: end of course reflection
---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("something that helped me with the learning process is regularly committing in order to stay consistent and easily go back if I mess up");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("one thing i would change about this course is for it to be a semester long course in order for people to stay in pace with the class");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("To be honest I don't know what I was expecting to learn because im fairly new to computers");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("everything is new to me in this course because these are my first coding classes");
